89372bdb62cfed940a341b00fad71391ed2dda7e
vilicvane <i@vilic.info>
2017-03-20 03:25:26 +0800

    Add project dir detection

--- src/cli/config.ts
+++ src/cli/config.ts
@@ -1,5 +1,8 @@
 import * as Path from 'path';
 
-export const BASEMAN_DIR = Path.resolve('test/baseman');
+import { getProjectDir } from '../internal-util';
+
+export const PROJECT_DIR = getProjectDir(Path.resolve());
+export const BASEMAN_DIR = Path.join(PROJECT_DIR, 'test/baseman');
 export const BASELINE_DIR = Path.join(BASEMAN_DIR, 'baseline');
 export const OUTPUT_DIR = Path.join(BASEMAN_DIR, 'output');
--- src/internal-util/fs.ts
+++ src/internal-util/fs.ts
@@ -1,8 +1,32 @@
 import * as FS from 'fs';
+import * as Path from 'path';
 
 import { ExpectedError } from 'clime';
 import * as v from 'villa';
 
 export async function isDirEmpty(dir: string): Promise<boolean> {
   return !(await v.call(FS.readdir, dir)).length;
+}
+
+export function getProjectDir(startDir: string): string {
+  let currentDir = startDir;
+  let lastDir: string | undefined;
+
+  while (lastDir !== currentDir) {
+    let path = Path.join(currentDir, 'package.json');
+    let stats: FS.Stats | undefined;
+
+    try {
+      stats = FS.statSync(path);
+    } catch (error) { }
+
+    if (stats && stats.isFile()) {
+      return currentDir;
+    }
+
+    lastDir = currentDir;
+    currentDir = Path.dirname(currentDir);
+  }
+
+  return startDir;
 }
